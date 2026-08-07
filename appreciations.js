cf9e5d6e3371f016d5ebbddf746980abda5bd51f
GudaoTeam <gudao251119@gmail.com>
2026-08-07 16:50:17 +0800

    Update appreciations.js

--- appreciations.js
+++ appreciations.js
@@ -6,7 +6,7 @@
 */
 window.GUDAO_APPRECIATIONS = [
   {
-    code: "GD Team LED",
+    code: "GuDao Team",
     name: "寬血蟹 × 大鋼牙",
     category: "培育紀錄",
     description: "血斑加入狂刺，經典嚴龍披上血色紋理，您不容錯過",
@@ -16,7 +16,7 @@ window.GUDAO_APPRECIATIONS = [
     instagram: "https://www.instagram.com/gudao.team/"
   },
   {
-    code: "GD Team LED",
+    code: "GuDao Team",
     name: "三宅雷神 × 妖炎",
     category: "培育紀錄",
     description: "孤島團隊精心培育交種個體",
@@ -26,7 +26,7 @@ window.GUDAO_APPRECIATIONS = [
     instagram: "https://www.instagram.com/gudao.team/"
   },
   {
-    code: "GD Team LED",
+    code: "GuDao Team",
     name: "妖炎 × 美系嚴龍",
     category: "收藏紀錄",
     description: "特選個體表現，主刺曲、葉片緊湊",
@@ -36,7 +36,7 @@ window.GUDAO_APPRECIATIONS = [
     instagram: "https://www.instagram.com/gudao.team/"
   },
   {
-    code: "GD Team SUN",
+    code: "GuDao Team",
     name: "孤島綠皮實生",
     category: "個體賞析",
     description: "不容錯過的好表現，團隊持續培育觀察",
