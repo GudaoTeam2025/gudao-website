ad03130da1fe8e4c3a688a6d3c2dbd19f2ec3a50
GudaoTeam2025 <gudao251119@gmail.com>
2026-08-07 13:16:50 +0800

    Add files via upload

--- products.js
+++ products.js
@@ -1,37 +1,5 @@
-/*
-  更新現貨只需要修改這個檔案。
-  照片請放進 GitHub 的 images/products/ 資料夾。
-  建議檔名只用英文小寫、數字與連字號，例如 agave-001.jpg。
-*/
-window.GUDAO_PRODUCTS = [
-  {
-    code: "GDA-001",
-    name: "龍舌蘭個體 001",
-    description: "請在這裡填寫品種、特色、培育方式與植株狀況。",
-    size: "株幅：約 15 cm",
-    price: "NT$ 1,800",
-    status: "現貨",
-    image: "images/products/agave-001.jpg",
-    instagram: "https://www.instagram.com/gudao.team/"
-  },
-  {
-    code: "GDA-002",
-    name: "龍舌蘭個體 002",
-    description: "請在這裡填寫品種、特色、培育方式與植株狀況。",
-    size: "株幅：約 18 cm",
-    price: "NT$ 2,200",
-    status: "現貨",
-    image: "images/products/agave-002.jpg",
-    instagram: "https://www.instagram.com/gudao.team/"
-  },
-  {
-    code: "GDA-003",
-    name: "龍舌蘭個體 003",
-    description: "已售出的個體可以繼續展示，作為孤島的歷史收藏紀錄。",
-    size: "株幅：約 16 cm",
-    price: "已售出",
-    status: "已售出",
-    image: "images/products/agave-003.jpg",
-    instagram: "https://www.instagram.com/gudao.team/"
-  }
+const GUDAO_PRODUCTS=[
+{code:"GDA-001",name:"妖炎 × 姬嚴龍",description:"母本參考與販售植株合成圖。",size:"請私訊確認",price:"DM for Price",status:"現貨",image:"images/products/agave-001.jpg"},
+{code:"GDA-002",name:"龍舌蘭個體 002",description:"請填寫品種、特色與植株狀況。",size:"請私訊確認",price:"DM for Price",status:"現貨",image:"images/products/agave-002.jpg"},
+{code:"GDA-003",name:"龍舌蘭個體 003",description:"過往個體紀錄。",size:"請私訊確認",price:"已售出",status:"已售出",image:"images/products/agave-003.jpg"}
 ];
