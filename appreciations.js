7b6cff4ef99cec5bda4668434bf262dc8a40bfda
GudaoTeam2025 <gudao251119@gmail.com>
2026-08-07 15:45:35 +0800

    Update appreciations.js

--- appreciations.js
+++ appreciations.js
@@ -6,33 +6,43 @@
 */
 window.GUDAO_APPRECIATIONS = [
   {
-    code: "GDA-A001",
-    name: "孤島賞析 001",
-    category: "個體賞析",
-    description: "填寫葉型、刺型、色澤、紋理與個體特色。",
-    cultivation: "室內燈養",
-    record: "持續記錄",
-    image: "images/appreciation/appreciation-001.jpg",
-    instagram: "https://www.instagram.com/gudao.team/"
-  },
-  {
-    code: "GDA-A002",
-    name: "孤島賞析 002",
+    code: "GD Team LED",
+    name: "寬血蟹 × 大鋼牙",
     category: "培育紀錄",
-    description: "填寫不同生長階段、控養變化與培育觀察。",
+    description: "血斑加入狂刺，經典嚴龍披上血色紋理，您不容錯過",
     cultivation: "室內燈養",
     record: "持續記錄",
-    image: "images/appreciation/appreciation-002.jpg",
+    image: "images/appreciation/appreciation-001.png",
     instagram: "https://www.instagram.com/gudao.team/"
   },
   {
-    code: "GDA-A003",
-    name: "孤島賞析 003",
+    code: "GD Team LED",
+    name: "三宅雷神 × 妖炎",
+    category: "培育紀錄",
+    description: "孤島團隊精心培育交種個體",
+    cultivation: "室內燈養",
+    record: "持續記錄",
+    image: "images/appreciation/appreciation-002.png",
+    instagram: "https://www.instagram.com/gudao.team/"
+  },
+  {
+    code: "GD Team LED",
+    name: "妖炎 × 美系嚴龍",
     category: "收藏紀錄",
-    description: "填寫收藏來源、個體表現與值得記錄的細節。",
+    description: "特選個體表現，主刺曲、葉片緊湊",
     cultivation: "孤島收藏",
     record: "典藏個體",
     image: "images/appreciation/appreciation-003.png",
     instagram: "https://www.instagram.com/gudao.team/"
-  }
+  },
+  {
+    code: "GD Team SUN",
+    name: "孤島綠皮實生",
+    category: "個體賞析",
+    description: "不容錯過的好表現，團隊持續培育觀察",
+    cultivation: "孤島收藏",
+    record: "典藏個體",
+    image: "images/appreciation/appreciation-004.png",
+    instagram: "https://www.instagram.com/gudao.team/"
+  },
 ];
