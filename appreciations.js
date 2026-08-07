29b8bb3ab1127ca282b53a422246676ca58b81c8
GudaoTeam2025 <gudao251119@gmail.com>
2026-08-07 14:46:22 +0800

    Add files via upload

---
+++ appreciations.js
@@ -0,0 +1,38 @@
+/*
+  孤島賞析資料
+  1. 圖片放在 images/appreciation/
+  2. 圖片建議 9:16，尺寸 1080 x 1920 px
+  3. 新增賞析時，複製一整組 { ... } 並修改內容
+*/
+window.GUDAO_APPRECIATIONS = [
+  {
+    code: "GDA-A001",
+    name: "孤島賞析 001",
+    category: "個體賞析",
+    description: "填寫葉型、刺型、色澤、紋理與個體特色。",
+    cultivation: "室內燈養",
+    record: "持續記錄",
+    image: "images/appreciation/appreciation-001.jpg",
+    instagram: "https://www.instagram.com/gudao.team/"
+  },
+  {
+    code: "GDA-A002",
+    name: "孤島賞析 002",
+    category: "培育紀錄",
+    description: "填寫不同生長階段、控養變化與培育觀察。",
+    cultivation: "室內燈養",
+    record: "持續記錄",
+    image: "images/appreciation/appreciation-002.jpg",
+    instagram: "https://www.instagram.com/gudao.team/"
+  },
+  {
+    code: "GDA-A003",
+    name: "孤島賞析 003",
+    category: "收藏紀錄",
+    description: "填寫收藏來源、個體表現與值得記錄的細節。",
+    cultivation: "孤島收藏",
+    record: "典藏個體",
+    image: "images/appreciation/appreciation-003.png",
+    instagram: "https://www.instagram.com/gudao.team/"
+  }
+];
