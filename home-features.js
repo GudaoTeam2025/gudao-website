04fbe2392637b244ee15293dd4286f02bcb5fa7b
GudaoTeam <gudao251119@gmail.com>
2026-08-10 14:29:13 +0800

    Add files via upload

---
+++ home-features.js
@@ -0,0 +1,43 @@
+/*
+  首頁精選卡片資料
+  圖片建議：9:16，1080 x 1920 px
+  圖片放置：images/home-showcase/
+
+  可編輯欄位：
+  status       左上角標籤
+  eyebrow      卡片英文分類
+  title        中文標題
+  description  圖片下方說明
+  image        圖片路徑
+  link         詳細說明連結，可使用內頁或外部網址
+  linkText     連結顯示文字
+*/
+window.GUDAO_HOME_FEATURES = [
+  {
+    status: "進行中",
+    eyebrow: "AVAILABLE NOW",
+    title: "現貨個體",
+    description: "時間淬鍊的作品，歡迎查看本期現貨與個體相關說明。",
+    image: "images/home-showcase/home-stock-001.jpg",
+    link: "stock.html",
+    linkText: "查看現貨說明"
+  },
+  {
+    status: "進行中",
+    eyebrow: "EVENTS",
+    title: "近期活動",
+    description: "市集、燈島巡迴、玩家交流與最新活動資訊。",
+    image: "images/home-showcase/home-event-001.jpg",
+    link: "event.html",
+    linkText: "查看活動說明"
+  },
+  {
+    status: "持續更新",
+    eyebrow: "APPRECIATION",
+    title: "個體賞析",
+    description: "記錄個體的表現、特色與培育過程中的每一份驚喜。",
+    image: "images/home-showcase/home-appreciation-001.jpg",
+    link: "appreciation.html",
+    linkText: "查看個體說明"
+  }
+];
