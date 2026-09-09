23e3ba126ca956f1ae0f572cd3613c19e2a0d449
GudaoTeam <gudao251119@gmail.com>
2026-09-09 16:33:18 +0800

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 /* 只填入可公開值。不可填入 service_role、sb_secret_、資料庫密碼或 Admin 密碼。 */
 window.GUDAO_SUPABASE_CONFIG = {
-  url: "請填入_SUPABASE_PROJECT_URL",
-  publishableKey: "請填入_SUPABASE_PUBLISHABLE_KEY",
+  url: "https://fojuvdkbdocgzqbvdtsd.supabase.co/rest/v1/",
+  publishableKey: "sb_publishable_jH5cR49oAHnoiRWOAAa-jw_nXJkRIFg",
   eventId: "023f72d4-e2b1-4f97-8541-895b026e4ab1"
 };
