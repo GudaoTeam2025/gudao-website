d90e2b89d35aa6024c4128ff20e4d199edb4117e
GudaoTeam <gudao251119@gmail.com>
2026-09-11 09:35:32 +0800

    Add files via upload

--- appreciations.js
+++ appreciations.js
@@ -1,48 +1,75 @@
 /*
-  孤島賞析資料
+  孤島植賞資料
+  ==================================================
   1. 圖片放在 images/appreciation/
-  2. 圖片建議 9:16，尺寸 1080 x 1920 px
-  3. 新增賞析時，複製一整組 { ... } 並修改內容
+  2. 每個個體可放 4 至 6 張圖片
+  3. 圖片可混用 4:6 與 9:16，頁面會完整顯示、不裁切
+  4. category 只使用：個體賞析、培育紀錄
+  5. 新增賞析時，複製一整組 { ... } 並修改內容
+  6. instagram 只填純網址，不要貼入 <a> HTML 標籤
 */
 window.GUDAO_APPRECIATIONS = [
   {
     code: "GuDao Team",
     name: "寬血蟹 × 大鋼牙",
     category: "培育紀錄",
-    description: "血斑加入狂刺，經典嚴龍披上血色紋理，您不容錯過",
+    description: "血斑加入狂刺，經典嚴龍披上血色紋理，持續記錄個體後續表現。",
     cultivation: "室內燈養",
     record: "持續記錄",
-    image: "images/appreciation/appreciation-001.png",
+    images: [
+      "images/appreciation/appreciation-001-01.png",
+      "images/appreciation/appreciation-001-02.png",
+      "images/appreciation/appreciation-001-03.png",
+      "images/appreciation/appreciation-001-04.png"
+    ],
     instagram: "https://www.instagram.com/gudao.team/"
   },
   {
     code: "GuDao Team",
     name: "三宅雷神 × 妖炎",
     category: "培育紀錄",
-    description: "孤島團隊精心培育交種個體",
+    description: "孤島團隊精心培育交種個體，依不同階段持續保存葉型與刺型變化。",
     cultivation: "室內燈養",
     record: "持續記錄",
-    image: "images/appreciation/appreciation-002.png",
+    images: [
+      "images/appreciation/appreciation-002-01.png",
+      "images/appreciation/appreciation-002-02.png",
+      "images/appreciation/appreciation-002-03.png",
+      "images/appreciation/appreciation-002-04.png",
+      "images/appreciation/appreciation-002-05.png"
+    ],
     instagram: "https://www.instagram.com/gudao.team/"
   },
   {
     code: "GuDao Team",
     name: "妖炎 × 美系嚴龍",
-    category: "收藏紀錄",
-    description: "特選個體表現，主刺曲、葉片緊湊",
+    category: "個體賞析",
+    description: "特選個體表現，主刺曲、葉片緊湊，記錄不同角度下的完整型態。",
     cultivation: "孤島收藏",
     record: "典藏個體",
-    image: "images/appreciation/appreciation-003.png",
+    images: [
+      "images/appreciation/appreciation-003-01.png",
+      "images/appreciation/appreciation-003-02.png",
+      "images/appreciation/appreciation-003-03.png",
+      "images/appreciation/appreciation-003-04.png",
+      "images/appreciation/appreciation-003-05.png",
+      "images/appreciation/appreciation-003-06.png"
+    ],
     instagram: "https://www.instagram.com/gudao.team/"
   },
   {
     code: "GuDao Team",
     name: "孤島綠皮實生",
     category: "個體賞析",
-    description: "不容錯過的好表現，團隊持續培育觀察",
+    description: "表現穩定的實生個體，透過多角度影像呈現葉幅、株型與刺座細節。",
     cultivation: "孤島收藏",
     record: "典藏個體",
-    image: "images/appreciation/appreciation-004.png",
+    images: [
+      "images/appreciation/appreciation-004-01.png",
+      "images/appreciation/appreciation-004-02.png",
+      "images/appreciation/appreciation-004-03.png",
+      "images/appreciation/appreciation-004-04.png"
+    ],
     instagram: "https://www.instagram.com/gudao.team/"
-  },
+  }
 ];
