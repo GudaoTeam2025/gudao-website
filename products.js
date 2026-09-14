7427172656378f4195eb1b0840f1411acb027907
GudaoTeam <gudao251119@gmail.com>
2026-09-14 10:27:02 +0800

    Update products.js

--- products.js
+++ products.js
@@ -1,1 +1,30 @@
-window.GUDAO_PRODUCTS=[{code:"2026.08.07",name:"妖炎 × 姬嚴龍",description:"母本參考與販售植株",size:"請私訊確認",price:"DM for Price",status:"現貨",image:"images/products/agave-001.jpg"},{code:"2026.08.07",name:"日系甲蟹 × 妖炎",description:"母本參考與販售植株",size:"請私訊確認",price:"DM for Price",status:"現貨",image:"images/products/agave-002.jpg"}];
+/*
+  孤島商品資料
+
+  新增商品方式：
+  1. 複製一整組 { ... }
+  2. 修改商品資料
+  3. 每組商品之間要保留逗號
+  4. 圖片放在 images/products/
+*/
+
+window.GUDAO_PRODUCTS = [
+  {
+    code: "2026.09.14",
+    name: "圓葉拇指",
+    description: "販售植株與角度參考",
+    size: "請私訊確認",
+    price: "DM for Price",
+    status: "現貨",
+    image: "images/products/agave-001.jpg"
+  },
+  {
+    code: "2026.08.07",
+    name: "日系甲蟹 × 妖炎",
+    description: "母本參考與販售植株",
+    size: "請私訊確認",
+    price: "DM for Price",
+    status: "現貨",
+    image: "images/products/agave-002.jpg"
+  }
+];
